e9d48c8d296fcfe50ebcb4090571c50737af7efd
Bastien Barcaioni <bastien.barcaioni@gmail.com>
2019-05-05 14:13:32 +0200

    closes #30

--- db/schema.sql
+++ db/schema.sql
@@ -13,12 +13,20 @@ create table maps (
     , data json
 );
 
+create table corporations (
+    corporation_id serial primary key
+    , map_id integer references maps on delete cascade 
+    , data json
+    , name varchar(50)
+);
+
 create table cities (
     city_id serial primary key 
     , map_id integer references maps default NULL on delete cascade
     , city_name varchar(50) 
     , updated_at timestamp  without time zone default (now() at time zone 'utc')
     , data json 
+    , corporation_id integer references corporations on delete set NULL default NULL
 );
 
 create table neighbouring_cities (
